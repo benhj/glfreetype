92e8890f99e3934dc67398650e2203e06a4ba7d6
benhj <bhj.research@gmail.com>
2018-03-01 17:23:35 +0000

    small refactors; code tidy up

--- src/TextRenderer.cpp
+++ src/TextRenderer.cpp
@@ -15,12 +15,11 @@
 #include <vector>
 
 // All ideas here ripped from a NEHE tutorial
-// I can take absolutely no credit
 // http://nehe.gamedev.net/tutorial/freetype_fonts_in_opengl/24001/
 
 namespace glfreetype {
-    // This Function Gets The First Power Of 2 >= The
-    // Int That We Pass It.
+    // Gets the first power of 2 >= 
+    // for the given int  
     inline int next_p2 (int a )
     {
         int rval=1;
@@ -29,43 +28,29 @@ namespace glfreetype {
         return rval;
     }
 
-    // Create A Display List Corresponding To The Given Character.
-    void make_dlist ( FT_Face face, char ch, GLuint list_base, GLuint * tex_base ) {
-     
-        // The First Thing We Do Is Get FreeType To Render Our Character
-        // Into A Bitmap. This Actually Requires A Couple Of FreeType Commands:
-     
-        // Load The Glyph For Our Character.
+    FT_BitmapGlyph generateBitmapForFace(FT_Face face, char ch)
+    {
+        // Load the character glyph.
         if(FT_Load_Glyph( face, FT_Get_Char_Index( face, ch ), FT_LOAD_DEFAULT ))
             throw std::runtime_error("FT_Load_Glyph failed");
      
-        // Move The Face's Glyph Into A Glyph Object.
+        // Move into a glyph object
         FT_Glyph glyph;
         if(FT_Get_Glyph( face->glyph, &glyph ))
             throw std::runtime_error("FT_Get_Glyph failed");
      
-        // Convert The Glyph To A Bitmap.
+        // Convert to a bitmap
         FT_Glyph_To_Bitmap( &glyph, ft_render_mode_normal, 0, 1 );
-        FT_BitmapGlyph bitmap_glyph = (FT_BitmapGlyph)glyph;
-     
-        // This Reference Will Make Accessing The Bitmap Easier.
-        FT_Bitmap& bitmap=bitmap_glyph->bitmap;
+        return (FT_BitmapGlyph)glyph;
+    }
 
-        // Use Our Helper Function To Get The Widths Of
-        // The Bitmap Data That We Will Need In Order To Create
-        // Our Texture.
-        int width = next_p2( bitmap.width );
-        int height = next_p2( bitmap.rows ) ;
-
-        // Allocate Memory For The Texture Data.
-        std::vector<GLubyte> expanded_data(2 * width * height, 0);
-
-        // Here We Fill In The Data For The Expanded Bitmap.
-        // Notice That We Are Using A Two Channel Bitmap (One For
-        // Channel Luminosity And One For Alpha).
-        // We Use The ?: Operator To Say That Value Which We Use
-        // Will Be 0 If We Are In The Padding Zone, And Whatever
-        // Is The FreeType Bitmap Otherwise.
+    void storeTextureData(int const width, 
+                          int const height,
+                          FT_Bitmap & bitmap,
+                          std::vector<GLubyte> & expanded_data)
+    {
+        // Note: two channel bitmap (One for
+        // channel luminosity and one for alpha).
         for(int j = 0; j < height ; j++) {
             for(int i = 0; i < width; i++) {
                 expanded_data[2 * (i + j * width)] = 255; // luminosity
@@ -74,8 +59,28 @@ namespace glfreetype {
                 bitmap.buffer[i + bitmap.width * j];
             }
         }
+    }
 
-        // Now We Just Setup Some Texture Parameters.
+    // Create A Display List Corresponding To The Given Character.
+    void make_dlist ( FT_Face face, char ch, GLuint list_base, GLuint * tex_base ) {
+     
+        // Retrieve a bitmap for the given char glyph.
+        FT_BitmapGlyph bitmap_glyph = generateBitmapForFace(face, ch);
+     
+        // This Reference Will Make Accessing The Bitmap Easier.
+        FT_Bitmap& bitmap=bitmap_glyph->bitmap;
+
+        // Get correct dimensions for bitmap
+        int width = next_p2( bitmap.width );
+        int height = next_p2( bitmap.rows ) ;
+
+        // Use a vector to store texture data (better than a raw array).
+        std::vector<GLubyte> expanded_data(2 * width * height, 0);
+
+        // Does what it says
+        storeTextureData(width, height, bitmap, expanded_data);
+
+        // Texture parameters.
         glBindTexture( GL_TEXTURE_2D, tex_base[ch]);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -84,44 +89,27 @@ namespace glfreetype {
          
         glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 
-        // Here We Actually Create The Texture Itself, Notice
-        // That We Are Using GL_LUMINANCE_ALPHA To Indicate That
-        // We Are Using 2 Channel Data.
+        // Create the texture Itself
+        // GL_LUMINANCE_ALPHA to indicate 2 channel data.
         glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0,
             GL_LUMINANCE_ALPHA, GL_UNSIGNED_BYTE, &expanded_data.front() );
 
-        // Now We Create The Display List
+        // Create the display list
         glNewList(list_base+ch,GL_COMPILE);
      
         glBindTexture(GL_TEXTURE_2D,tex_base[ch]);
      
         glPushMatrix();
      
-        // First We Need To Move Over A Little So That
-        // The Character Has The Right Amount Of Space
-        // Between It And The One Before It.
+        // Ensure right amount of space for each new char
         glTranslatef(bitmap_glyph->left,0,0);
-     
-        // Now We Move Down A Little In The Case That The
-        // Bitmap Extends Past The Bottom Of The Line
-        // This Is Only True For Characters Like 'g' Or 'y'.
         glTranslatef(0,bitmap_glyph->top-(bitmap.rows * 0.9),0);
      
-        // Now We Need To Account For The Fact That Many Of
-        // Our Textures Are Filled With Empty Padding Space.
-        // We Figure What Portion Of The Texture Is Used By
-        // The Actual Character And Store That Information In
-        // The x And y Variables, Then When We Draw The
-        // Quad, We Will Only Reference The Parts Of The Texture
-        // That Contains The Character Itself.
+        // Account for padding.
         float x = (float)bitmap.width / (float)width,
               y = (float)bitmap.rows / (float)height;
      
-        // Here We Draw The Texturemapped Quads.
-        // The Bitmap That We Got From FreeType Was Not
-        // Oriented Quite Like We Would Like It To Be,
-        // But We Link The Texture To The Quad
-        // In Such A Way That The Result Will Be Properly Aligned.
+        // Draw the texturemapped quads.
         glBegin(GL_QUADS);
         glTexCoord2d(0,0); glVertex2f(0,bitmap.rows);
         glTexCoord2d(0,y); glVertex2f(0,0);
@@ -129,12 +117,10 @@ namespace glfreetype {
         glTexCoord2d(x,0); glVertex2f(bitmap.width,bitmap.rows);
         glEnd();
         glPopMatrix();
+
+        // Move over for next character
         glTranslatef(face->glyph->advance.x >> 6 ,0,0);
-     
-        // Increment The Raster Position As If We Were A Bitmap Font.
-        // (Only Needed If You Want To Calculate Text Length)
-        // glBitmap(0,0,0,0,face->glyph->advance.x >> 6,0,NULL);
-     
+
         // Finish The Display List
         glEndList();
     }
